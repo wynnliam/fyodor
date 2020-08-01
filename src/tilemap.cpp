7e9dfc45ac31eb074eeb79d15344ff12f31e1c85
Liam Wynn <wynnliam@pdx.edu>
2020-07-31 20:46:16 -0700

    Added (incomplete) constructor to tilemap class

--- src/tilemap.cpp
+++ src/tilemap.cpp
@@ -1,3 +1,8 @@
 // Liam Wynn, 7/31/2020, Fyodor
 
 #include "./tilemap.h"
+
+using namespace fyodor;
+
+tilemap::tilemap(unsigned int w, unsigned int h) : width(w), height(h) {
+}
--- src/tilemap.h
+++ src/tilemap.h
@@ -5,6 +5,9 @@
 	1. rendering
 	2. walls - which we won't do for awhile because there
 	is no reason for handling collisions right now.
+
+	To do this, I want to define a vertex buffer object
+	that this will encapsulate.
 */
 
 #ifndef TILEMAP
@@ -13,7 +16,8 @@
 namespace fyodor {
 	class tilemap {
 		public:
-			
+			tilemap(unsigned int w, unsigned int h);
+
 		private:
 			// Describes the dimensions of the map
 			unsigned int width, height;
