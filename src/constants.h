5d42130c1b571e0c7de68d9adfbb2e052adc3d6e
Liam Wynn <wynnliam@pdx.edu>
2021-07-24 12:44:07 -0700

    Added constants.h. Updated tile_atlas to use it

---
+++ src/constants.h
@@ -0,0 +1,16 @@
+// Liam Wynn, 7/24/2021, Fyodor
+
+/*
+  This file defines all constants that are relevent to
+  Fyodor.
+*/
+
+#ifndef CONSTANTS
+#define CONSTANTS
+
+namespace fyodor {
+  // Size of a tile in pixels.
+  const unsigned int TILE_SIZE  = 16;
+}
+
+#endif
--- src/tile_atlas.cpp
+++ src/tile_atlas.cpp
@@ -1,17 +1,14 @@
 // Liam Wynn, 8/18/2020, Fyodor
 
 #include "./tile_atlas.h"
+#include "./constants.h"
 
 using namespace std;
 using namespace fyodor;
 
-// TODO: Move to constants class
-// Size of a tile in pixels.
-const unsigned int TILE_SIZE = 16;
-
 tile_atlas::tile_atlas(shared_ptr<texture> tex) : map_tex(tex) {
-	num_rows = map_tex->get_height() / 16;
-	num_cols = map_tex->get_width() / 16;
+	num_rows = map_tex->get_height() / TILE_SIZE;
+	num_cols = map_tex->get_width() / TILE_SIZE;
 }
 
 vector<glm::vec2> tile_atlas::uv_coords(const unsigned int texture_index) {
